58669b504f3bba210dc6a8cb48c1134e2b7dd565
Ilze <ilzezane.radzinna@gmail.com>
2015-03-06 16:56:12 +0000

    add server ip view

--- web.py
+++ web.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
+    
+@app.route("/ping")
+def hello():
+    return "pong!"
+    
+  @app.route("/myip")
+  def myip():
+      return requests.get('http://eth0.me').content
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
